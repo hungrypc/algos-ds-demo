2323c7550fac781f15b2447148b13dec0c6c6d6a
hpc <philipchanhk626@gmail.com>
2020-05-14 14:01:48 -0400

    testing react-code-blocks

--- src/pages/Patterns.js
+++ src/pages/Patterns.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import { CodeBlock, dracula } from 'react-code-blocks'
 
 function Patterns() {
+
+  const code = ` function test(arr) {
+    for (let i = 0; i < arr.length; i++) {
+      console.log('wow')
+    }
+  };
+  `;
+
   return (
     <div>
       PATTERNS
+      <div className="demo">
+        <CodeBlock
+        text={code}
+        language={'javascript'}
+        showLineNumbers={true}
+        theme={dracula}
+        highlight="2, 3, 4"
+        wrapLines
+      />
+      </div>
     </div>
   )
 }
