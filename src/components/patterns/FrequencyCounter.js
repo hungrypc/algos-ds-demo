f0e6ebbf3c046786b9ca31d2104eaa37d315dbcc
hpc <philipchanhk626@gmail.com>
2020-05-14 20:48:20 -0400

    adding explanation for each step, index is working better than i expected;

--- src/components/patterns/FrequencyCounter.js
+++ src/components/patterns/FrequencyCounter.js
@@ -4,8 +4,20 @@ import Code from '../Code';
 function FrequencyCounter() {
   const [highlight, setHighlight] = useState("");
   const [idx, setIdx] = useState(0);
-  const highlightArr = ["", "1", "2", "3"]
-  // let idx = 0;
+  const highlightArr = ["", "2, 3, 4", "5, 6", "7, 8, 9", "8", "8", "8", "10, 11, 12", "11", "11", "11"]
+  const explanation = {
+    0: "",
+    1: "First, check if the length of arr1 is equal to the length of arr2. The condition for returning true cannot happen if both elements don't have the same number of elements, so we return false if that is the case.",
+    2: "Initialize two object variables in preparation to keep track of each value in both arrays. These will be our frequency counters.",
+    3: "Now that we have our frequency counters set up, we can start putting them to use. We'll start by iterating through arr1.",
+    4: "As we iterate through every element in arr1, we can keep track of its value by storing it in our frequencyCount1 object.",
+    5: "As we iterate through every element in arr1, we can keep track of its value by storing it in our frequencyCount1 object.",
+    6: "As we iterate through every element in arr1, we can keep track of its value by storing it in our frequencyCount1 object.",
+    7: "We then do the same with arr2, this time keeping track of each value by storing it in our frequencyCount2 object.",
+    8: "We then do the same with arr2, this time keeping track of each value by storing it in our frequencyCount2 object.",
+    9: "We then do the same with arr2, this time keeping track of each value by storing it in our frequencyCount2 object.",
+    10: "We then do the same with arr2, this time keeping track of each value by storing it in our frequencyCount2 object.",
+  }
 
   const code = `function same(arr1, arr2) {
   if (arr1.length !== arr2.length) {
@@ -32,8 +44,6 @@ function FrequencyCounter() {
   `;
 
   function onNext() {
-    console.log(idx)
-    console.log(highlight)
     if (idx >= 0 && idx < highlightArr.length - 1) {
       let temp = idx;
       temp += 1
@@ -58,11 +68,17 @@ function FrequencyCounter() {
         Problem Solving Patterns
       </h1>
       <div className="ui container">
-        <h3 className="ui block header">
+        <h2 className="ui block header">
           Frequency Counter
-        </h3>
+        </h2>
         <div>
-          Using objects or sets to collect values/frequencies of values.
+          This is when we use objects or sets to keep track of certain values or the frequencies of those values.
+        </div>
+        <div className="prompt">
+          <i className="far fa-hand-point-right"></i>
+          <div>
+            Write a function called same, which accepts two arrays. <br/> The function should return true if every value in the array has its corresponding value squared in the second array. The frequency of values must be the same.
+          </div>
         </div>
         <div className="ui segment">
           <div className="ui two column grid">
@@ -70,7 +86,7 @@ function FrequencyCounter() {
               <Code code={code} highlight={highlight} />
             </div>
             <div className="column">
-              <p></p>
+              <p>{explanation[idx]}</p>
               <p></p>
               <p></p>
               <p></p>
