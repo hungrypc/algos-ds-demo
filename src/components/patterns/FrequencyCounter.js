a788062d2ebd7bd312f94b07b60f72c8fddcccaa
hpc <philipchanhk626@gmail.com>
2020-05-20 14:26:52 -0400

    steps for frequency counter

--- src/components/patterns/FrequencyCounter.js
+++ src/components/patterns/FrequencyCounter.js
@@ -5,22 +5,21 @@ import Breakdown from '../Breakdown';
 function FrequencyCounter() {
   const [currentIdx, setCurrentIdx] = useState(0)
 
-  const highlightArr = ["", "2, 3, 4", "5, 6", "7, 8, 9", "8", "10, 11, 12", "13", "14, 15, 16", "17, 18, 19", "21"];
+  const highlightArr = ["", "2, 3, 4", "5, 6", "7, 8, 9", "7, 8, 9", "7, 8, 9", "10, 11, 12", "10, 11, 12", "10, 11, 12", "13", "14, 15, 16", "17, 18, 19", "21"];
   const explanationText = {
     0: "",
     1: "First, check if the length of arr1 is equal to the length of arr2. The condition for returning true cannot happen if both elements don't have the same number of elements, so we return false if that is the case.",
     2: "Initialize two object variables in preparation to keep track of each value in both arrays. These will be our frequency counters.",
-    3: "Now that we have our frequency counters set up, we can start putting them to use by iterating through arr1.",
-    4: "As we loop through every element in arr1, we can keep track of its value by storing it in our frequencyCount1 object.",
-    5: "As we loop through every element in arr1, we can keep track of its value by storing it in our frequencyCount1 object.",
-    6: "As we loop through every element in arr1, we can keep track of its value by storing it in our frequencyCount1 object.",
+    3: "Iterate through arr1 so that as we loop through every element, we can keep track of its value by storing it in our frequencyCount1 object.",
+    4: "Iterate through arr1 so that as we loop through every element, we can keep track of its value by storing it in our frequencyCount1 object.",
+    5: "Iterate through arr1 so that as we loop through every element, we can keep track of its value by storing it in our frequencyCount1 object.",
+    6: "We then do the same with arr2, this time keeping track of each value of arr2 by storing it in our frequencyCount2 object.",
     7: "We then do the same with arr2, this time keeping track of each value of arr2 by storing it in our frequencyCount2 object.",
     8: "We then do the same with arr2, this time keeping track of each value of arr2 by storing it in our frequencyCount2 object.",
-    9: "We then do the same with arr2, this time keeping track of each value of arr2 by storing it in our frequencyCount2 object.",
-    10: "For the last step, we'll loop through each key in our frequencyCount1 object to check for the conditions for the function to return true.",
-    11: "If the key's value squared does not exist in the frequencyCount2 object, then there's no point in continuing so we immediately return false.",
-    12: "If there are two elements with the same value in arr1, there needs to be two elements in arr2 with that value squared. So, if the values don't correspond, we return false.",
-    13: "Finally, if we haven't hit any conditions that return false, we reach the end of the code and return true."
+    9: "For the last step, we'll loop through each key in our frequencyCount1 object to check for the conditions for the function to return true.",
+    10: "If the key's value squared does not exist in the frequencyCount2 object, then there's no point in continuing so we immediately return false.",
+    11: "If there are two elements with the same value in arr1, there needs to be two elements in arr2 with that value squared. So, if the values don't correspond, we return false.",
+    12: "Finally, if we haven't hit any conditions that return false, we reach the end of the code and return true."
   };
 
   const code = `function same(arr1, arr2) {
@@ -52,9 +51,9 @@ function FrequencyCounter() {
   const prompt = `Write a function called same, which accepts two arrays.
   The function should return true if every value in the array has its corresponding value squared in the second array. The frequency of values must be the same.`;
 
-  function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
+  // function sleep(ms) {
+  //   return new Promise(resolve => setTimeout(resolve, ms));
+  // }
 
   let x = 82;
 
@@ -71,29 +70,43 @@ function FrequencyCounter() {
     p.draw = async function () {
       p.background(255)
       p.fill(0)
-      p.text(`arr1 = ${arr1}`, 0, 15)
-      p.text(`arr2 = ${arr2}`, 180, 15)
+      if (currentIdx > 0) {
+        p.text(`arr1 = ${arr1}`, 0, 15)
+        p.text(`arr2 = ${arr2}`, 180, 15)        
+      }
       if (currentIdx > 1) {
         p.text('frequencyCount1 = {              }', 0, 55)
-        p.text('frequencyCount2 = {              }', 0, 85)
+        p.text('frequencyCount2 = {              }', 0, 95)
       }
       if (currentIdx > 2) {           
         p.noStroke()
         p.fill(19, 194, 194);
         p.ellipse(x, 25, 7, 7);
-      }
-      if (currentIdx === 3) x = 82
-      if (currentIdx >= 4) {
+      }    
+      if (currentIdx >= 3) {
+        x = 82
         p.fill(0)
         p.text('1:1,', 190, 56)
       }
-      if (currentIdx >= 5) {
+      if (currentIdx >= 4) {
         x = 110
         p.text('2:1,', 235, 56)
       }
-      if (currentIdx >= 6) {
+      if (currentIdx >= 5) {
         x = 140
         p.text('3:1', 280, 56)
+      }
+      if (currentIdx >= 6) {
+        x = 262.7
+        p.text('4:1,', 190, 96)
+      }
+      if (currentIdx >= 7) {
+        x = 290.6
+        p.text('1:1,', 235, 96)
+      }
+      if (currentIdx >= 8) {
+        x = 319.5
+        p.text('9:1', 280, 96)
       }
     };
   };
@@ -120,7 +133,7 @@ function FrequencyCounter() {
       prevClick={prevClick}
     >
       <div className="explanation__visual">
-        <div>same([1, 2, 3], [4, 1, 9]);</div>
+        <div>e.g. same([1, 2, 3], [4, 1, 9]);</div>
         <div>
           <P5Wrapper sketch={sketch} />
         </div>
