fe6788b75e98772ef8e1de9284fbf88be4a8678a
hpc <philipchanhk626@gmail.com>
2020-05-29 10:12:48 -0400

    added doubly linked view code

--- src/components/linkedlists/DoublyLinked.js
+++ src/components/linkedlists/DoublyLinked.js
@@ -16,7 +16,7 @@ function DoublyLinked() {
   const dummy
 }`;
 
-  const brief = `Doubly Linked Lists are almost identical to a singly linked list, except every node has another pointer, to the previous node. 
+  const brief = `Doubly Linked Lists are almost identical to a singly linked list, except every node has another pointer to the previous node. 
 This allows for more flexibility, but the trade off is that it also takes up more memory.`;
 
   const prompt = `Write a function that ...`;
@@ -62,6 +62,131 @@ class DoublyLinkedList {
     this.head = null
     this.tail = null
     this.length = 0
+  }
+
+  push(val) {
+    const newNode = new Node(val)
+    if (!this.head) {
+      this.head = newNode
+      this.tail = newNode
+    } else {
+      this.tail.next = newNode
+      newNode.prev = this.tail
+      this.tail = newNode
+    }
+    this.length++
+    return this
+  }
+
+  pop() {
+    if (this.length === 0) return undefined
+    const poppedNode = this.tail
+    if (this.length === 1) {
+      this.head = null
+      this.tail = null
+    } else {
+      this.tail = poppedNode.prev
+      this.tail.next = null
+      poppedNode.prev = null
+    }
+    this.length--
+    return poppedNode
+  }
+
+  shift() {
+    if (this.length === 0) return undefined
+    const oldHead = this.head
+    if (this.length === 1) {
+      this.head = null
+      this.tail = null
+    } else {
+      this.head = oldHead.next
+      this.head.prev = null
+      oldHead.next = null
+    }
+    this.length--
+    return oldHead
+  }
+
+  unshift(val) {
+    const newNode = new Node(val)
+    if (this.length === 0) {
+      this.head = newNode
+      this.tail = newNode
+    } else {
+      this.head.prev = newNode
+      newNode.next = this.head
+      this.head = newNode
+    }
+    this.length++
+    return this
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.length) return null
+    let count, current
+    if (index <= this.length / 2) {
+      count = 0
+      current = this.head
+      while (count !== index) {
+        current = current.next
+        count++
+      }
+      return current
+    } else {
+      count = this.length - 1
+      current = this.tail
+      while (count !== index) {
+        current = current.prev
+        count--
+      }
+      return current
+    }
+  }
+
+  set(index, val) {
+    const node = this.get(index)
+    if (node) {
+      node.val = val
+      return true
+    }
+    return false
+  }
+
+  insert(index, val) {
+    if (index < 0 || index >= this.length) return false
+    if (index === 0) return !!this.unshift(val)
+    if (index === this.length - 1) return !!this.push(val)
+
+    let beforeNode = this.get(index - 1)
+    let afterNode = beforeNode.next
+    let newNode = new Node(val)
+
+    beforeNode.next = newNode
+    newNode.prev = beforeNode
+    afterNode.prev = newNode
+    newNode.next = afterNode
+
+    this.length++
+    return true
+  }
+
+  remove(index) {
+    if (index < 0 || index >= this.length) return undefined
+    if (index === 0) return !!this.shift()
+    if (index === this.length - 1) return !!this.pop()
+
+    let removed = this.get(index)
+    let beforeRemoved = removed.prev
+    let afterRemoved = removed.next
+    
+    beforeRemoved.next = afterRemoved
+    afterRemoved.prev = beforeRemoved
+    removed.next = null
+    removed.prev = null
+
+    this.length--
+    return removed
   }
 }`
 
