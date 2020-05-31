127796abc4bbcc48b9ace976eed1b9220e5daeb0
hpc <philipchanhk626@gmail.com>
2020-05-31 11:46:47 -0400

    added code and explanation for stacks and queues

--- src/components/stacksqueues/Queue.js
+++ src/components/stacksqueues/Queue.js
@@ -1,6 +1,36 @@
 import React from 'react'
+import Code from '../Code'
 
 function Queue() {
+  const code = `class Stack {
+    constructor() {
+      this.front = null
+      this.rear = null
+      this.size = 0
+    }
+  
+    enqueue(val) {
+      const newNode = new Node(val)
+      if (!this.front) {
+        this.front = newNode
+        this.rear = newNode
+      } else {
+        this.last.next = newNode
+        this.last = newNode
+      }
+      return ++this.size
+    }
+  
+    dequeue() {
+      if (!this.front) return null
+      const temp = this.front
+      if (this.size === 1) this.rear = null
+      this.front = temp.next
+      this.size--
+      return temp.value
+    }
+  }`
+
   return (
     <div>
       <div className="queue--details">
@@ -8,7 +38,9 @@ function Queue() {
           Queues
       </h2>
         <div className="queue--details__breakdown">
-          about queues
+          Queues are data structures that follow the First-In-First-Out (FIFO) principle. This means that the <strong>first</strong> item inserted into a stack is the <strong>first</strong> one to be deleted.
+          <br /><br />Elements can be added only from the rear, and elements be deleted only from the front. Think of queues like a queue of people waiting in line for something. The first person to queue up is the first person served.
+          <br />The insertion of an element in a queue is called an enqueue operation and deleting an element is called a dequeue operation.
       </div>
         {/* <div className="prompt">
         <i className="far fa-hand-point-right"></i>
@@ -17,8 +49,15 @@ function Queue() {
         </div>
       </div> */}
         <div className="ui segment">
-          segment
-      </div>
+          <div className="ui two column grid">
+            <div className="column demo">
+              <Code code={code} />
+            </div>
+            <div className="column">
+              p5
+          </div>
+          </div>
+        </div>
       </div>
     </div>
   )
--- src/components/stacksqueues/Stack.js
+++ src/components/stacksqueues/Stack.js
@@ -1,13 +1,44 @@
 import React from 'react'
+import Code from '../Code'
 
 function Stack() {
+  const code = `class Stack {
+  constructor() {
+    this.top = null
+    this.size = 0
+  }
+
+  push(val) {
+    const newNode = new Node(val)
+    if (!this.top) {
+      this.top = newNode
+    } else {
+      const temp = this.top
+      this.top = newNode
+      newNode.next = temp
+    }
+    return ++this.size
+  }
+
+  pop() {
+    if (!this.top) return null
+    const temp = this.top
+    this.top = temp.next
+    this.size--
+    return temp.value
+  }
+}`
+
   return (
     <div className="stack--details">
       <h2 className="ui block header">
         Stacks
       </h2>
-      <div className="stack--details__breakdown">
+      <div className="stack--details__breakdown pre-line">
         Stacks are data structures that follow the Last-In-First-Out (LIFO) principle. This means that the <strong>last</strong> item inserted into a stack is the <strong>first</strong> one to be deleted.
+        <br /><br />Elements can be added and deleted only from the top. We can think of them like a stack of books; you can remove only the top of the book, and you can add a new book on the top. In order to retrieve the third book in the stack,
+        you would have to take the fifth book off, then the fourth book, until you are able to retrieve the third book.
+        <br /> The insertion of an element into stack is called pushing. Deletion of an element from the stack is called popping.
       </div>
       {/* <div className="prompt">
         <i className="far fa-hand-point-right"></i>
@@ -16,7 +47,14 @@ function Stack() {
         </div>
       </div> */}
       <div className="ui segment">
-        segment
+        <div className="ui two column grid">
+          <div className="column demo">
+            <Code code={code} />
+          </div>
+          <div className="column">
+            p5
+          </div>
+        </div>
       </div>
     </div>
   )
