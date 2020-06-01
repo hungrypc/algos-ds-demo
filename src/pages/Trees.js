68b788f138e010f770e39cd0e7a61f130ab35152
hpc <philipchanhk626@gmail.com>
2020-06-01 12:00:40 -0400

    some reorganizing tree page

--- src/pages/Trees.js
+++ src/pages/Trees.js
@@ -4,6 +4,7 @@ import BFS from '../components/trees/BFS'
 import DFS from '../components/trees/DFS'
 
 function Trees() {
+
   return (
     <div className="trees page ui container padded">
       <h1 className="ui dividing header page-header">
@@ -15,16 +16,36 @@ function Trees() {
           <li>Each element can only have 2 children, we typically name them the left and right child.</li>
           <li>There has to be one entry point (only one root).</li>
         </ul>
-        <h3 className="ui header">Tree terminology</h3>
-        <ul>
-          <li><strong>Root:</strong> The top node in a tree</li>
-          <li><strong>Child:</strong> A node directly connected to another node when moving away from the root</li>
-          <li><strong>Parent:</strong> The converse notion of a child</li>
-          <li><strong>Sibling:</strong> A group of nodes with the same parent</li>
-          <li><strong>Leaf:</strong> A node with no children</li>
+        <h3 className="ui header">Terminology</h3>
+        <ul>      
+          <li>
+            <div className="terminology">
+              <div className="terminology_term">Child:</div>
+              <div className="terminology_def">A node directly connected to another node when moving away from the root</div>
+            </div>
+          </li>
+          <li>
+            <div className="terminology">
+              <div className="terminology_term">Parent:</div>
+              <div className="terminology_def">The converse notion of a child</div>
+            </div>
+          </li>
+          <li>
+            <div className="terminology">
+              <div className="terminology_term">Sibling:</div>
+              <div className="terminology_def">A group of nodes with the same parent</div>
+            </div>
+          </li>
+          <li>
+            <div className="terminology">
+              <div className="terminology_term">Leaf:</div>
+              <div className="terminology_def">A node with no children</div>
+            </div>
+          </li>
         </ul>
       </div>
       <BinarySearchTree />
+      <br />
       <br />
       <div className="ui container">
         <h1 className="ui dividing header page-header">
